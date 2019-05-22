729d8055346f1cc9dc7fae3f1b6598bf9d507609
chenmychou <chenmychou@gmail.com>
2019-05-22 18:35:18 +0800

    node接口

--- node/index.js
+++ node/index.js
@@ -4,8 +4,8 @@ const bodyParser = require('koa-bodyparser')
 const config = require('./config')
 const serve = require('koa-static');
 const mongoose = require('mongoose')
-// const redis = require('redis')
-// const client = redis.createClient()
+const redis = require('redis')
+const client = redis.createClient()
 const app = new Koa()
 const koaBody = require('koa-body');
 const uploadRouter = require('./routes/uploadRouter')
@@ -23,14 +23,14 @@ mongoose.set('useNewUrlParser', true);
 mongoose.set('useFindAndModify', false);
 mongoose.set('useCreateIndex', true); // v5.4.13
 // mongoose.connect(config.dataBase, {useNewUrlParser: true}, (err, db) => {
-// mongoose.connect(config.dataBase, (err, db) => {
-//     if (err) return console.log(err)
-//     console.log('数据库连接')
-// })
+mongoose.connect(config.dataBase, (err, db) => {
+    if (err) return console.log(err)
+    console.log('数据库连接')
+})
 
-// client.on('ready', (err) => {
-//     console.log('ok! redis')
-// })
+client.on('ready', (err) => {
+    console.log('ok! redis')
+})
 app.use(cors());
 const ciyu_routers = require('./routes/ciyu_route')
 const words_routers = require('./routes/words_route')
@@ -44,4 +44,4 @@ app.use(xiehouyu_routers.routes()).use(xiehouyu_routers.allowedMethods())
 app.use(uploadRouter.routes()).use(uploadRouter.allowedMethods())
 
 app.listen(config.port)
-console.log('服务启动在 http://localhost:'+config.port)
+console.log('服务启动在 http://localhost:' + config.port)
